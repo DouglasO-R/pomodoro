20872ffc203ca2a873c2971897292599824dca11
douglasor <douglasor@outlook.com>
2022-10-20 16:18:24 -0300

    WIP: add Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,23 @@
+import { NavLink } from 'react-router-dom'
+import { Timer, Scroll } from 'phosphor-react'
+
+import { HeaderContainer } from './styles'
+import Logo from '../../assets/logo.svg'
+
 export function Header() {
   return (
-    <header>
-      <h1>HEADER</h1>
-    </header>
+    <HeaderContainer>
+      <img src={Logo} alt="Logotipo do ignite" />
+
+      <nav>
+        <NavLink to="/" end title="Timer">
+          <Timer size={24} />
+        </NavLink>
+
+        <NavLink to="/history" end title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
   )
 }
