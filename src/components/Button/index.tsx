2da6f5ba2e600f374d56646aad3f426876709673
douglasor <douglasor@outlook.com>
2022-11-23 20:19:36 -0300

    remove button component

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer } from './Button.styles'
-
-type Props = {
-  variant?: 'primary' | 'secondary' | 'success' | 'danger'
-}
-
-export function Button({ variant = 'primary' }: Props) {
-  return <ButtonContainer variant={variant}> Enviar</ButtonContainer>
-}
